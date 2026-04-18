ecde74ae7c61b8e8f2df5da7b719cd19d8447d78
rndmtrsh <t.maulanaakmal@gmail.com>
2026-04-19 00:14:21 +0700

    hi

---
+++ ros2_ws/src/pan_tilt_controller/pan_tilt_controller/first-node.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class
+
+def main(args=None):
+    #
+    
+
+if __name__ == '__main__':
+    rclpy.init(args=args)
+    main()
+    rclpy.shutdown()
